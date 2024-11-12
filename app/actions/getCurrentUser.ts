21525f2dc679756ac26f10ef6ab2d0998c029cd5
Oa Blom <oablom@gmail.com>
2024-11-12 11:28:36 +0100

    Refactor getCurrentUser function to enhance session validation and error handling

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,34 +1,36 @@
 import { getServerSession } from "next-auth/next";
+
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
 import prisma from "@/app/libs/prismadb";
 
-// Hämta sessionen och använd den för att få användardata
+export const dynamic = "force-dynamic";
+
 export async function getSession() {
-  const session = await getServerSession(authOptions);
-  if (!session?.user?.email) {
-    return null; // Om ingen session finns, returnera null
-  }
-  return session;
+  return await getServerSession(authOptions);
 }
 
 export default async function getCurrentUser() {
   try {
     const session = await getSession();
-    if (!session) {
-      return null; // Om session är null, returnera null
+
+    if (!session?.user?.email) {
+      return null;
     }
 
     const currentUser = await prisma.user.findUnique({
       where: {
-        email: session?.user?.email as string,
+        email: session.user.email as string,
       },
     });
+
+    console.log("Admin status i getCurrentUser:", currentUser?.isAdmin);
+    console.log("Current user:", currentUser);
 
     if (!currentUser) {
       return null;
     }
 
-    // Format och returnera användardata
     return {
       ...currentUser,
       isAdmin: currentUser.isAdmin,
@@ -36,8 +38,9 @@ export default async function getCurrentUser() {
       updatedAt: currentUser.updatedAt.toISOString(),
       emailVerified: currentUser.emailVerified?.toISOString() || null,
     };
-  } catch (error) {
-    console.error("Error fetching current user:", error);
-    throw new Error("Failed to fetch user");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
   }
 }
