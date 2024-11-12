579808b386a627145f2c4a4fc092418d33df7210
Oa Blom <oablom@gmail.com>
2024-11-12 10:32:26 +0100

    Move NotFoundPage implementation to 404.tsx and enable dynamic rendering with error logging

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,15 @@
 // _not-found.tsx for vercel rendering
-// "use client";
+"use client";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// import { useEffect } from "react";
-import EmptyState from "./components/EmptyState";
+import { useEffect } from "react";
+import EmptyState from "../app/components/EmptyState";
 
 const NotFoundPage = () => {
-  //   useEffect(() => {
-  //     console.error("Page not found");
-  //   }, []);
+  useEffect(() => {
+    console.error("Page not found");
+  }, []);
   return (
     <EmptyState
       title="Damnit, the page is missing!"
